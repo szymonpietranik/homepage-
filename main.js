516c0c2bb49169c666081665e4cbd1d147afb9bd
Szymon Pietranik <szymonpietranik20@gmail.com>
2020-05-17 18:35:14 +0200

    add style to main page

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-aler  ('hello')
-const name = 'Szymon';
+aler("hello");
+const name = "Szymon";
 const age = 24;
 console.log(name);
 console.log(age);
-console.log(`Siema, nazywam się ${name} i mam ${age} lata `)
-
+console.log(`Siema, nazywam się ${name} i mam ${age} lata `);
