8e811d1737166aff4ac12a239412a9ac089a9bba
Szymon Pietranik <szymonpietranik20@gmail.com>
2020-05-17 20:52:05 +0200

    add paragraph by js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-aler("hello");
+alert("hello");
 const name = "Szymon";
 const age = 24;
 console.log(name);
 console.log(age);
 console.log(`Siema, nazywam się ${name} i mam ${age} lata `);
+
+const emptyParagraph = document.querySelector('.week-summary__description--js');
+
+emptyParagraph.innerHTML = `Witaj Drogi Odwiedzający`;
