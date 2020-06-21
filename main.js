2d9f7687bcc6435e622c412ec3026916021cd149
Szymon Pietranik <szymonpietranik20@gmail.com>
2020-06-21 12:00:49 +0200

    add hamburger menu to main page

--- main.js
+++ main.js
@@ -5,6 +5,13 @@ console.log(name);
 console.log(age);
 console.log(`Siema, nazywam się ${name} i mam ${age} lata `);
 
-const emptyParagraph = document.querySelector('.week-summary__description--js');
+const emptyParagraph = document.querySelector(".week-summary__description--js");
 
 emptyParagraph.innerHTML = `Witaj Drogi Odwiedzający`;
+
+const hamburger = document.querySelector(".hamburger--js");
+
+hamburger.addEventListener("click", () => {
+  const nav = document.querySelector(".navigation--js");
+  nav.classList.toggle("navigation--open");
+});
